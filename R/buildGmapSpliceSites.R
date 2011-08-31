368dcf8597e062e6a2af91b092768b3a21eb6905
Cory Barr <barr.cory@gene.com>
2011-08-31 07:32:29 +0000

    added mods to building splice site indexes
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gmapR@57754 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/buildGmapSpliceSites.R
+++ R/buildGmapSpliceSites.R
@@ -1,2 +1,39 @@
-buildGmapSpliceSites <- function() {
+buildGmapSpliceSites <- function(splice_sites,
+                                 gsnap_data_dir,
+                                 genome,
+                                 splices_name) {
+  scratch_dir <- tempdir()
+  cur_dir <- getwd()
+  on.exit({unlink(scratch_dir, recursive=TRUE)
+           setwd(cur_dir)})
+  if(!file.exists(scratch_dir)) dir.create(scratch_dir)
+
+  splice_file <- file.path(scratch_dir, "splice_file.txt")
+  writeLines(splice_sites, con=splice_file)
+
+  iit_store <- file.path(globals()$gsnap_bin_dir, "iit_store")
+  splice_site_file <- paste(splices_name, ".splices", sep="")
+  setwd(scratch_dir)
+  sys_command <- paste("cat", splice_file, "|", iit_store, "-o", splice_site_file)
+  system(sys_command)
+
+  ##TODO: move this to the setup script for HTSeqGenieBase
+  ##if (genome=='hg19') {
+  ##  iit_dest_dir <- file.path(globals()$gsnap_save_dir, "hg19_ucsc", "hg19_ucsc.maps")
+  ##} else if (genome=='mm9') {
+  ##    iit_dest_dir <- file.path(globals()$gsnap_save_dir, "mm9", "mm9.maps") }
+  ##else {
+  ##  stop("genome not supported")
+  ##}
+
+  iit_dest_dir <- file.path(gsnap_data_dir,
+                            genome,
+                            paste(genome, ".maps", sep=""))
+
+  iit_file <- paste(splices_name, '.splices.iit', sep="")
+  
+  sys_command <- paste("cp", iit_file, iit_dest_dir)
+  system(sys_command)
+  
+  return(0)
 }
--- R/buildRefseqSlicesites.R
+++ R/buildRefseqSlicesites.R
@@ -17,37 +17,12 @@ gsnapBuildRefseqSplicesites <- function(genome) {
   query <- ucscTableQuery(session, "refGene")
   refGeneTable <- getTable(query)
 
-  
   ##exon_starts and exon_ends should be a single string comma-sep set
   ##of coordinates
   splice_site_lines <- exonsToGmapSpliceSites(refGeneTable)
 
-  scratch_dir <- file.path(getwd(), paste('tmp', ceiling(runif(1) * 1000000), sep='.'))
-  dir.create(scratch_dir)
-  splice_file <- file.path(scratch_dir, "splice_file.txt")
-
-  writeLines(splice_site_lines, splice_file)
-
-  iit_store <- file.path(globals()$gsnap_bin_dir, "iit_store")
-  splice_site_file <- 'refSeq.splices'
-  setwd(scratch_dir)
-  sys_command <- paste("cat", splice_file, "|", iit_store, "-o", splice_site_file)
-  system(sys_command)
-
-  if (genome=='hg19') {
-    iit_dest_dir <- file.path(globals()$gsnap_save_dir, "hg19_ucsc", "hg19_ucsc.maps") } else if (genome=='mm9') {
-      iit_dest_dir <- file.path(globals()$gsnap_save_dir, "mm9", "mm9.maps") } else {
-        stop("genome not supported")
-    }
-
-  iit_file <- 'refSeq.splices.iit'
+  if(buildGmapSpliceSites(splice_sites) != 0)
+    stop("Could not build Gmap splice site index")
   
-  sys_command <- paste("cp", iit_file, iit_dest_dir)
-  system(sys_command)
-
-  setwd('..')
-    
-  unlink(scratch_dir, recursive=T)
-
   return(0)
 }
