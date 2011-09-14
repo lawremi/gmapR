cee1c2615fcd3ea7febf8fa0f2e278960c14f9aa
Cory Barr <barr.cory@gene.com>
2011-09-14 10:06:49 +0000

    modifications in installGmap
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gmapR@58029 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/installGmap.R
+++ R/installGmap.R
@@ -3,32 +3,47 @@
 ##' 
 ##' @title Download and Install GMAP
 ##' @param install_dir directory where GMAP will be installed
+##' @param samtools_dir directory where samtools is installed
 ##' @return 0
 ##' @author Cory Barr
 ##' @export
-installGmap <- function(install_dir) {
+installGmap <- function(install_dir, samtools_dir=NULL) {
 
+  if (is.null(samtools_dir))
+    warning("Samtools directory not specified. Utilities to parse BAM files will be unavailable")
+  
+  if (!file.exists(install_dir))
+    dir.create(install_dir, recursive=TRUE)
   install_dir <- file_path_as_absolute(install_dir)
   
-  temp_dir <- 'temp'
+  temp_dir <- tempdir()
   if(file.exists(temp_dir))
     unlink(temp_dir, recursive=TRUE)
   dir.create(temp_dir)
+  on.exit(unlink(temp_dir, recursive=TRUE))
   setwd(temp_dir)
-  
-  system('wget http://research-pub.gene.com/gmap/src/gmap-gsnap-2011-03-28.tar.gz')
-  system('tar xzvf gmap-gsnap-2011-03-28.tar.gz')
+
+  gmap_zip <- 'gmap-gsnap-2011-09-09.tar.gz'
+  system(paste('wget',
+               file.path('http://research-pub.gene.com/gmap/src', gmap_zip)))
+  dir.create("unzip_dir")
+  system(paste('tar xzvf', gmap_zip, "-C unzip_dir"))  
   orig_dir <- getwd()
-  on.exit(setwd(orig_dir))
-  setwd('gmap-2011-03-28/')
+  gmap_src_dir <- dir("unzip_dir", full.names=TRUE)
+  setwd(gmap_src_dir)
   
   gsnap_dir <- file.path(install_dir, "gmap")
   gsnap_genomes_dir <- file.path(install_dir, "gmap", "genomes")
   dir.create(gsnap_genomes_dir, recursive=TRUE)
-  
+
+
+  stop("account for samtools")
   sys_call <- paste("./configure",
                     paste("prefix=", gsnap_dir, sep=''),
                     paste("with_gmapdb=", gsnap_genomes_dir, sep=''))
+  if (!is.null(samtools_dir))
+    sys_call <- paste(sys_call,
+                      paste("with_samtools=", samtools_dir, sep=""))
   system(sys_call)                  
   system('make')
   system('make install')
