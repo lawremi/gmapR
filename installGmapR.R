a00648578d833c08871d8df569e934dce9986c2a
Herve Pages <hpages@fhcrc.org>
2011-09-16 06:58:20 +0000

    Use dirname() instead of basename().
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gmapR@58134 bc3139a8-67e5-0310-9ffc-ced21a209358

--- installGmapR.R
+++ installGmapR.R
@@ -1,11 +1,11 @@
 library(gmapR)
 
-##a samtools installation is needed
+##path to samtools source folder
 samtools_dir <- '/gne/home/coryba/source/samtools/samtools-0.1.16/samtools'
 
 ##HTSeqGenieBase used to find location of gsnap binary. gmapR package should know its own location
 library(HTSeqGenieBase)
-gmap_install_dir <- basename(HTSeqGenieBase::globals()$gsnap_bin_dir)
+gmap_install_dir <- dirname(HTSeqGenieBase::globals()$gsnap_bin_dir)
 
 ##for testing
 ##gmap_install_dir <- '/gne/home/coryba/sandbox/gmapTestInstallDir'
@@ -14,7 +14,7 @@ gmap_install_dir <- basename(HTSeqGenieBase::globals()$gsnap_bin_dir)
 installGmap(gmap_install_dir, samtools_dir)
 
 ##NOTE: bin gets appended to gmap_install_dir, need to fix so it
-##actually installs where it's supposed to. That's why basename is
+##actually installs where it's supposed to. That's why dirname is
 ##called on globals()$gnap_bin_dir.
 
 ##build hg19 IIT
