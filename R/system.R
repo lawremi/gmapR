79899d1a3da6f5572320cfa2a5740828d304b520
Cory Barr <barr.cory@gene.com>
2012-08-16 18:50:30 +0000

    changes to commandLine()'s helper function that handles arguments with no default
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gmapR@68516 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/system.R
+++ R/system.R
@@ -7,17 +7,22 @@ getDefaultGmapPath <- function() {
   system.file("usr", "bin", package = "gmapR")
 }
 
-.commandLine.handleNameArgs <- function(defaultArgs, args) {
+.commandLine.handleMissingArgs <- function(defaultArgs, userArgs) {
   ##arguments with no default need to be handled explicity, since
-  ##objects of class "missing" cannot be eval'ed.
-  classes <- sapply(defaultArgs, class)
+  ##"missing" args cannot be eval'ed.
+  isMissing <- rep(NA, length(defaultArgs))
+  ##using a for loop because "sapply(defaultArgs, missing)" won't work
+  for (i in seq_along(isMissing)) {
+    x <- defaultArgs[[i]]
+    isMissing[i] <- missing(x)
+  }
   handledArgs <- defaultArgs
-  handledArgs[classes == "name"] <- "NoT_pRoViDeD"
+  handledArgs[isMissing] <- "NoT_pRoViDeD"
 
   ##verify function calling commandLine provided values for any
   ##argument with no default
-  callerMustSupply <- names(classes[classes == "name"])
-  if (any(class(args[callerMustSupply]) == "name")) {
+  callerMustSupply <- names(handledArgs[isMissing])
+  if (!all(callerMustSupply %in% names(userArgs))) {
     stop("An argument with a required value was not passed to commandLine()")
   }
 
@@ -29,14 +34,15 @@ commandLine <- function(binary = "gsnap",
                         path = getOption("gmap.path", getDefaultGmapPath()))
 {
   ##get values of arguments of function that called this function
-  args <- mget(names(formals(sys.function(sys.parent()))), parent.frame())
+  parentFormals <- formals(sys.function(sys.parent()))
+  userArgs <- mget(names(parentFormals), parent.frame())
 
   ##Some args cannot be passed simultaneously to gsnap. Remove arg if
   ##it is the default.
-  defaultArgs <- formals(sys.function(sys.parent()))
+  defaultArgs <- parentFormals
   ##objects of class "name" must be handled specially since they
   ##cannot be eval'ed
-  defaultArgs <- .commandLine.handleNameArgs(defaultArgs, args)
+  defaultArgs <- .commandLine.handleMissingArgs(defaultArgs, userArgs)
   parentEnv <- sys.parent()
   defaultArgs <- lapply(defaultArgs, eval, envir=parentEnv)  
   ##some args have a default vector with more than one element (for
@@ -45,24 +51,24 @@ commandLine <- function(binary = "gsnap",
                         function(x) {if (length(x) > 1L) x[[1L]] else x}
                         )
   ##remove defaults
-  isDefault <- mapply(identical, args, defaultArgs)
+  isDefault <- mapply(identical, userArgs, defaultArgs)
   if (sum(!isDefault > 0L)) {
-    args <- args[!isDefault]
+    userArgs <- userArgs[!isDefault]
   }
   
-  args <- Filter(Negate(is.null), args)
-  args <- Filter(function(x) !identical(x, FALSE), args)
-  named <- !grepl("^\\.", names(args))
-  unnamed_args <- sapply(args[!named], as.character)
-  named_args <- paste(ifelse(nchar(names(args[named])) > 1, "--", "-"),
-                            gsub("_", "-", names(args[named])), sep = "")
-  toggle_arg <- sapply(args[named], isTRUE)
-  named_args[!toggle_arg] <-
-    paste(named_args[!toggle_arg],
-          sapply(args[named][!toggle_arg], as.character))
+  userArgs <- Filter(Negate(is.null), userArgs)
+  userArgs <- Filter(function(x) !identical(x, FALSE), userArgs)
+  named <- !grepl("^\\.", names(userArgs))
+  unnamedUserArgs <- sapply(userArgs[!named], as.character)
+  namedUserArgs <- paste(ifelse(nchar(names(userArgs[named])) > 1, "--", "-"),
+                            gsub("_", "-", names(userArgs[named])), sep = "")
+  toggle_arg <- sapply(userArgs[named], isTRUE)
+  namedUserArgs[!toggle_arg] <-
+    paste(namedUserArgs[!toggle_arg],
+          sapply(userArgs[named][!toggle_arg], as.character))
   if (!is.null(path))
     binary <- file.path(path, binary)
-  paste(binary, paste(c(named_args, unnamed_args), collapse = " "))
+  paste(binary, paste(c(namedUserArgs, unnamedUserArgs), collapse = " "))
 }
 
 ## at some point, mxbay want to customize this
