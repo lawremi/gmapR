2d65a19f8c0a608fe59df231ea5201b633fc2773
Cory Barr <barr.cory@gene.com>
2012-08-23 22:24:29 +0000

    added test for GsnapParam
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gmapR@68746 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/test_GsnapParam.R
@@ -0,0 +1,17 @@
+test_GsnapParam_constructor <- function() {
+  
+  fa <- system.file("extdata/hg19.p53.fasta", package="gmapR", mustWork=TRUE)
+  fastaFile <- rtracklayer::FastaFile(fa)
+  gmapGenome <- GmapGenome(fastaFile, create=TRUE)
+
+  gsnapParam <- GsnapParam(genome = gmapGenome,
+                           unique_only = FALSE,
+                           max_mismatches = NULL,
+                           suboptimal_levels = 0L, mode = "standard",
+                           npaths = 10L,
+                           novelsplicing = FALSE, splicing = NULL, 
+                           nthreads = 1L,
+                           batch = "2")
+
+  checkTrue(is(gsnapParam, "GsnapParam"))
+}
