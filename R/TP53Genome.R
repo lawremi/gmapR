cc97426fe8a328e6f69ab11b1ce858e179079694
Michael Lawrence <lawremi@iastate.edu>
2012-09-07 16:50:33 +0000

    TP53Genome now includes the known splice sites (from UCSC knownGene)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gmapR@69199 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/TP53Genome.R
+++ R/TP53Genome.R
@@ -6,12 +6,18 @@ TP53Genome <- function() {
   eg <- org.Hs.eg.db::org.Hs.egSYMBOL2EG[[gene]]
   txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene::TxDb.Hsapiens.UCSC.hg19.knownGene
   tx <- transcripts(txdb, vals = list(gene_id = eg))
-  p53Seq <- getSeq(BSgenome.Hsapiens.UCSC.hg19::Hsapiens, range(tx) + 1e6,
+  roi <- range(tx) + 1e6
+  
+  p53Seq <- getSeq(BSgenome.Hsapiens.UCSC.hg19::Hsapiens, roi,
                    as.character = FALSE)
   names(p53Seq) <- gene
   genome <- GmapGenome(genome = p53Seq,
                        name = paste0(gene, "_demo"),
                        create = TRUE)
-  spliceSites(genome, "") <- txdb
+  
+  exons <- shift(subsetByOverlaps(exonsBy(txdb), roi), 1L - start(roi))
+  exons <- renameSeqlevels(exons, setNames(gene, seqnames(roi)))
+  spliceSites(genome, "knownGene") <- exons
+  
   genome
 }
