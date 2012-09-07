bad75f2baa03cbc8e14793300d227cd0f4f34208
Michael Lawrence <lawremi@iastate.edu>
2012-09-07 17:16:41 +0000

    If TP53Genome has already been created, do not bother loading all of the packages, generating the splice sites, etc.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gmapR@69205 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/TP53Genome.R
+++ R/TP53Genome.R
@@ -1,23 +1,29 @@
 TP53Genome <- function() {
-  checkPackageInstalled("org.Hs.eg.db", required = TRUE)
-  checkPackageInstalled("TxDb.Hsapiens.UCSC.hg19.knownGene", required = TRUE)
-  checkPackageInstalled("BSgenome.Hsapiens.UCSC.hg19", required = TRUE)
   gene <- "TP53"
-  eg <- org.Hs.eg.db::org.Hs.egSYMBOL2EG[[gene]]
-  txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene::TxDb.Hsapiens.UCSC.hg19.knownGene
-  tx <- transcripts(txdb, vals = list(gene_id = eg))
-  roi <- range(tx) + 1e6
+  genomeName <- paste0(gene, "_demo")
   
-  p53Seq <- getSeq(BSgenome.Hsapiens.UCSC.hg19::Hsapiens, roi,
-                   as.character = FALSE)
-  names(p53Seq) <- gene
-  genome <- GmapGenome(genome = p53Seq,
-                       name = paste0(gene, "_demo"),
-                       create = TRUE)
-  
-  exons <- shift(subsetByOverlaps(exonsBy(txdb), roi), 1L - start(roi))
-  exons <- renameSeqlevels(exons, setNames(gene, seqnames(roi)))
-  spliceSites(genome, "knownGene") <- exons
-  
-  genome
+  if (genomeName %in% genome(GmapGenomeDirectory())) {
+    GmapGenome(genomeName)
+  } else {
+    checkPackageInstalled("org.Hs.eg.db", required = TRUE)
+    checkPackageInstalled("TxDb.Hsapiens.UCSC.hg19.knownGene", required = TRUE)
+    checkPackageInstalled("BSgenome.Hsapiens.UCSC.hg19", required = TRUE)
+    eg <- org.Hs.eg.db::org.Hs.egSYMBOL2EG[[gene]]
+    txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene::TxDb.Hsapiens.UCSC.hg19.knownGene
+    tx <- transcripts(txdb, vals = list(gene_id = eg))
+    roi <- range(tx) + 1e6
+    
+    p53Seq <- getSeq(BSgenome.Hsapiens.UCSC.hg19::Hsapiens, roi,
+                     as.character = FALSE)
+    names(p53Seq) <- gene
+    genome <- GmapGenome(genome = p53Seq,
+                         name = genomeName,
+                         create = TRUE)
+    
+    exons <- shift(subsetByOverlaps(exonsBy(txdb), roi), 1L - start(roi))
+    exons <- renameSeqlevels(exons, setNames(gene, seqnames(roi)))
+    spliceSites(genome, "knownGene") <- exons
+    
+    genome
+  }
 }
