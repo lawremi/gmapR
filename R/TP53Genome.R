9dfa7ad7cabf4a141858e007ebd0da7dc94ec3db
Cory Barr <barr.cory@gene.com>
2012-09-10 22:52:34 +0000

    added patches supplied by Greg
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gmapR@69292 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/TP53Genome.R
+++ R/TP53Genome.R
@@ -2,16 +2,16 @@ TP53Genome <- function() {
   gene <- "TP53"
   genomeName <- paste0(gene, "_demo")
   
-  if (genomeName %in% genome(GmapGenomeDirectory())) {
+  if (genomeName %in% genome(GmapGenomeDirectory(create=TRUE))) {
     GmapGenome(genomeName)
-  } else {
+  } else{ 
     checkPackageInstalled("org.Hs.eg.db", required = TRUE)
     checkPackageInstalled("TxDb.Hsapiens.UCSC.hg19.knownGene", required = TRUE)
     checkPackageInstalled("BSgenome.Hsapiens.UCSC.hg19", required = TRUE)
-    eg <- org.Hs.eg.db::org.Hs.egSYMBOL2EG[[gene]]
     txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene::TxDb.Hsapiens.UCSC.hg19.knownGene
-    tx <- transcripts(txdb, vals = list(gene_id = eg))
-    roi <- range(tx) + 1e6
+
+    ## get region of interest
+    roi <- getGeneRoi(gene)
     
     p53Seq <- getSeq(BSgenome.Hsapiens.UCSC.hg19::Hsapiens, roi,
                      as.character = FALSE)
@@ -20,10 +20,23 @@ TP53Genome <- function() {
                          name = genomeName,
                          create = TRUE)
     
-    exons <- shift(subsetByOverlaps(exonsBy(txdb), roi), 1L - start(roi))
-    exons <- renameSeqlevels(exons, setNames(gene, seqnames(roi)))
+    exons <- subsetRegion(exonsBy(txdb), roi, gene)
     spliceSites(genome, "knownGene") <- exons
     
     genome
   }
+}
+
+getGeneRoi <- function(gene, extend=1e6) {
+  txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene::TxDb.Hsapiens.UCSC.hg19.knownGene
+  eg <- org.Hs.eg.db::org.Hs.egSYMBOL2EG[[gene]]
+  tx <- transcripts(txdb, vals = list(gene_id=eg))
+  range(tx) + extend
+}
+
+subsetRegion <- function(x, roi, newseqname) {
+  x <- shift(subsetByOverlaps(x, roi), 1L - start(roi))
+  x <- renameSeqlevels(x, setNames(newseqname, seqnames(roi)))
+  seqlengths(x)[newseqname] <- width(roi)
+  x
 }
--- R/utils.R
+++ R/utils.R
@@ -12,6 +12,14 @@ checkPackageVersion <- function(pkg, version, required = FALSE) {
   success
 }
 
+##' Checks if a given package is installed.
+##'
+##' @title Checks if a given package is installed.
+##' @param pkg A character string containing a package name.
+##' @param required A boolean. The functions stops if set to \code{TRUE} and if the required package is not present. Default is \code{FALSE}.
+##' @return A boolean.
+##' @author Cory Barr
+##' @export
 checkPackageInstalled <- function(pkg, required = FALSE) {
   checkPackageVersion(pkg, "0.0.0", required)
 }
