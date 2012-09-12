c6574d2b6bd6fa5a61b9218d4eb4f11efb32c732
Cory Barr <barr.cory@gene.com>
2012-09-12 18:11:41 +0000

    tests updated since if DNAStringSet obj is now used to create a GmapGenome, DNAStringSet obj needs names() set
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gmapR@69377 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_GmapGenome.R
+++ inst/unitTests/test_GmapGenome.R
@@ -1,11 +1,13 @@
 test_GmapGenome_constructor_DNAStringSet_noCreate <- function() {
   dna <- Biostrings::DNAStringSet("ACTGTGTCAG")
-  gmapGenome <- GmapGenome(genome=dna, name="thing")
+  names(dna) <- "test"
+  gmapGenome <- GmapGenome(genome=dna, name="thing", create=FALSE)
   checkTrue(is(gmapGenome, "GmapGenome"))
 }
 
 test_GmapGenome_constructor_DNAStringSet_create <- function() {
   dna <- Biostrings::DNAStringSet("ACTGTGTCAG")
+  names(dna) <- "test"
   genomeDir <- file.path(tempdir(), as.integer(runif(1) * 1000000000))
   if (file.exists(genomeDir)) unlink(genomeDir, recursive=TRUE)
   dir.create(genomeDir, recursive=TRUE)
@@ -40,14 +42,13 @@ testGmapGenome_constructor_FastaFile_create <- function() {
 test_GmapGenome_accessors <- function() {
   genomeName <- "testGenome"
   dna <- Biostrings::DNAStringSet("ACTGTGTCAG")
+  names(dna) <- "testDNAString"
   genomeDir <- file.path(tempdir(), as.integer(runif(1) * 1000000000))
   if (file.exists(genomeDir)) unlink(genomeDir, recursive=TRUE)
   dir.create(genomeDir, recursive=TRUE)
-  
   on.exit(unlink(genomeDir, recursive=TRUE))
   gmapGenome <- GmapGenome(genome=dna, directory=genomeDir,
-                           name=genomeName, create=TRUE)
-
+                           name=genomeName, create=FALSE)
   checkIdentical(path(gmapGenome), file.path(genomeDir, genomeName))
   checkTrue(is(directory(gmapGenome), "GmapGenomeDirectory"))
   checkIdentical(genome(gmapGenome), genomeName)
