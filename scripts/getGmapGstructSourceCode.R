ad42d27c0e96d48e5af46475af1cde3990c2d493
Cory Barr <barr.cory@gene.com>
2012-10-03 23:02:14 +0000

    updated script to add GMAP/GSNAP/GSTRUCT src from SVN repo
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gmapR@70171 bc3139a8-67e5-0310-9ffc-ced21a209358

--- scripts/getGmapGstructSourceCode.R
+++ scripts/getGmapGstructSourceCode.R
@@ -1,26 +1,74 @@
-bootstrapAndExtract <- function(projectSVNURL, extractDir, program) {
+##run this to update. The current working directory needs to be the
+##top-level directory of the SVN checkout
+updateGMAPRSrc <- function() {
+  gmapSVNProj <- "http://resscm/bioinfo/projects/gmap/branches/internal-2011-12-28"
+  gstructSVNProj <- "http://resscm/bioinfo/projects/gstruct/trunk"
 
+  extractDirGmap <- file.path(getwd(), "src/gmap")
+  extractDirGstruct <- file.path(getwd(), "src/gstruct")
+
+  bootstrapAndExtract(projectSVNURL=gmapSVNProj, extractDir=extractDirGmap,
+                      program="gmap")  
+  bootstrapAndExtract(projectSVNURL=gstructSVNProj, extractDir=extractDirGstruct,
+                      program="gstruct")
+  .copySamflagsHeader(extractDirGstruct, extractDirGmap)
+}
+  
+bootstrapAndExtract <- function(projectSVNURL, extractDir, program) {
   startingDir <- getwd()
   on.exit(setwd(startingDir))
-  
+
+  svnCheckoutDir <- .getSVNProj(projectSVNURL, extractDir)
+  on.exit(unlink(svnCheckoutDir, recursive=TRUE), add=TRUE)
+  setwd(svnCheckoutDir)
+  .bootstrapSVNCheckout()  
+  .configureSrc(program)
+  .makeDist()
+  .extractDistTarballIntoSrcDirectory(extractDir)
+  invisible(TRUE)
+}
+
+###################
+###helper functions
+###################
+
+.copySamflagsHeader <- function(extractDirGstruct, extractDirGmap) {
+  ##gstruct needs samflags.h. Copying from the gmap src
+  gstructSamflagsLoc <- file.path(extractDirGstruct, "src/samflags.h")
+  if (!file.exists(gstructSamflagsLoc)) {
+    
+    gmapSamflagsLoc <- file.path(extractDirGmap, "src/samflags.h")  
+    if (!file.exists(gmapSamflagsLoc)) {
+      stop("Could not find the samflags.h file in the gmap src code")
+    }
+    
+    file.copy(gmapSamflagsLoc, gstructSamflagsLoc)
+  }
+}
+
+.getSVNProj <- function(projectSVNURL, extractDir) {
   ##grab from svn
   tmpDir <- file.path(tempdir(), basename(extractDir))
   dir.create(tmpDir, recursive=TRUE)
-  on.exit(unlink(tmpDir, recursive=TRUE), add=TRUE)
-  
   command <- paste("svn co",
                    projectSVNURL,
                    tmpDir)
   if (!system(command) == 0) {
     stop("Could not check out project from SVN")
   }
-    
-  ##bootstrap it
-  setwd(tmpDir)
+
+  return(tmpDir)
+}
+
+##assumes in the correct dir
+.bootstrapSVNCheckout <- function() {
   if (!system("./bootstrap.Rdist") == 0) {
     stop("unable to bootstrap")
   }
+}
 
+
+.configureSrc <- function(program) {
   ##--with-gmapdb=${GMAPDB} --prefix=${PREFIX}
   ##configure. Set
   ##run a "make dist" to build a tarball
@@ -37,22 +85,29 @@ bootstrapAndExtract <- function(projectSVNURL, extractDir, program) {
       system("touch config.site")
     }
   }
-  
+}
+
+.makeDist <- function() {
   ##run a "make dist" to build a tarball
   if (!system("make distcheck") == 0) {
     stop("unable to 'make dist'")
   }  
-  
+}
+
+.extractDistTarballIntoSrcDirectory <- function(extractDir) {
   ##extract tarball into 'src' directory of package
-  distTarball <- dir(pattern="*\\.tar\\.gz$")
+  distTarball <- dir(pattern="*\\.tar\\.gz$", full.names=TRUE)
   if (length(distTarball) > 1) {
     stop("Found more than one tarball in directory. ",
          "Not sure which is the distribution. Aborting.")
   }
-  if (!file.exists(extractDir)) {
-    dir.create(extractDir, recursive=TRUE)
+  distTarball <- tools::file_path_as_absolute(distTarball)
+  if (file.exists(extractDir)) {
+    unlink(extractDir, recursive=TRUE)
   }
-  distName <- sub("\\..*", "", distTarball)
+  dir.create(extractDir, recursive=TRUE)
+
+  distName <- sub("\\..*", "", basename(distTarball))
   command <- paste("tar xvf", distTarball,
                    "-C",
                    extractDir,
@@ -71,31 +126,4 @@ bootstrapAndExtract <- function(projectSVNURL, extractDir, program) {
     stop("Could not move extracted directory down one level")
   }
   unlink(whereTarExtracted, recursive=TRUE)
-
-  invisible(TRUE)
 }
-
-options(error=recover)
-
-gmapSVNProj <- "http://resscm/bioinfo/projects/gmap/branches/internal-2011-12-28"
-extractDirGmap <- file.path(getwd(), "src/gmap")
-bootstrapAndExtract(projectSVNURL=gmapSVNProj, extractDir=extractDirGmap,
-                    program="gmap")
-
-gstructSVNProj <- "http://resscm/bioinfo/projects/gstruct/trunk"
-extractDirGstruct <- file.path(getwd(), "src/gstruct")
-bootstrapAndExtract(projectSVNURL=gstructSVNProj, extractDir=extractDirGstruct,
-                    program="gstruct")
-
-##gstruct needs samflags.h. Copying from the gmap src
-gstructSamflagsLoc <- file.path(extractDirGstruct, "src/samflags.h")
-if (!file.exists(gstructSamflagsLoc)) {
- 
-  gmapSamflagsLoc <- file.path(extractDirGmap, "src/samflags.h")  
-  if (!file.exists(gmapSamflagsLoc)) {
-    stop("Could not find the samflags.h file in the gmap src code")
-  }
-
-  file.copy(gmapSamflagsLoc, gstructSamflagsLoc)
-}
-    
