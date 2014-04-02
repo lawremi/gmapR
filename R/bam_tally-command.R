ed089f79c951eabc0968d15a68d98f1bba173859
Michael Lawrence <lawremi@iastate.edu>
2014-04-02 21:12:52 +0000

    ensure that seqlevel ordering from genome is preserved in result
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gmapR@88338 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bam_tally-command.R
+++ R/bam_tally-command.R
@@ -124,9 +124,9 @@ variantSummary <- function(x, read_pos_breaks = NULL, high_base_quality = 0L,
                      seqlengths = seqlengths(genome)))
   mcols(gr) <- metacols
   checkTallyConsistency(gr)
-  si <- keepSeqlevels(merge(seqinfo(bamFile(x)), seqinfo(genome)),
-                      seqlevels(bamFile(x)))
-  seqlevels(gr) <- seqlevels(si) # reorder if necessary
+  ## important to preserve seqlevel ordering compatible with 'genome'
+  si <- merge(seqinfo(genome), seqinfo(bamFile(x)))
+  si <- keepSeqlevels(si, intersect(seqlevels(si), seqlevels(bamFile(x))))
   seqinfo(gr) <- si
   gr <- normalizeIndelAlleles(gr, genome)
   gr
