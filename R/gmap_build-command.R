b8e2b22ebdd50d91bb13e6f5fbb78287f7e280e3
Michael Lawrence <lawremi@iastate.edu>
2015-11-20 12:24:09 +0000

    let users pass a 'param' when building a GmapGenome from a FaFile
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gmapR@110762 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/gmap_build-command.R
+++ R/gmap_build-command.R
@@ -30,9 +30,10 @@ setMethod("gmap_build", c("RTLFile", "GmapGenome"), function(x, genome, ...) {
   gmap_build(import(x), genome, ...)
 })
 
-setMethod("gmap_build", c("FaFile", "GmapGenome"), function(x, genome, ...) {
-  gmap_build(getSeq(x), genome, ...)
-})
+setMethod("gmap_build", c("FaFile", "GmapGenome"),
+          function(x, genome, param, ...) {
+              gmap_build(getSeq(x, param), genome, ...)
+          })
 
 setMethod("gmap_build", c("DNAStringSet", "GmapGenome"),
           function(x, genome, ...) {
